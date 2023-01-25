4d37b6feb86a6818d7bbc3fc14119ceee4a7efd5
Michael Kotliar <misha.kotliar@gmail.com>
2023-01-25 14:22:28 -0500

    Change the logic how we keep "ccpca" reduction

--- tools/dockerfiles/scripts/sc_tools/modules/analyses.R
+++ tools/dockerfiles/scripts/sc_tools/modules/analyses.R
@@ -487,7 +487,11 @@ rna_analyze <- function(seurat_data, args, cell_cycle_data=NULL){
     SeuratObject::DefaultAssay(seurat_data) <- "RNA"                            # safety measure
     SeuratObject::Idents(seurat_data) <- "new.ident"                            # safety measure
     backup_reductions <- c()                                                    # RNA integration main remove atac related reductions so we need to back them up
-    for (reduction_name in c("ccpca", "atac_lsi", "atacumap", "wnnumap")){
+    reduction_names <- c("atac_lsi", "atacumap", "wnnumap")
+    if (is.null(cell_cycle_data)){                                              # we are not planning to run cell cycle score assignment, so we want to keep "ccpca"
+        reduction_names <- append(reduction_names, "ccpca")
+    }
+    for (reduction_name in reduction_names){
         if (reduction_name %in% names(seurat_data@reductions)){
             base::print(base::paste("Backing up reduction", reduction_name))
             backup_reductions[[reduction_name]] <- seurat_data[[reduction_name]]
